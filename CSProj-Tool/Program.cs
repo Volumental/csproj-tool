eebcd13e04e4df3bee181fd26a0684d49f17e06e
Samuel Carlsson <samuel.carlsson@volumental.com>
2014-12-03 17:00:31 +0100

    Now possible to add references to assembly files

--- CSProj-Tool/Program.cs
+++ CSProj-Tool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Microsoft.Build.BuildEngine;
 using System;
@@ -14,35 +15,65 @@ namespace csproj.tool
 
         static int Main(string[] args)
         {
-            if (args.Count() < 1)
+            if (args.Count() < 2)
             {
-                Console.WriteLine("Usage: csproj-tool <csproj file> <project-references-to-remove>");
+                Console.WriteLine("Usage: csproj-tool add|remove <csproj file> <data ...>");
                 return 1;
             }
-
-            var projectFilePath = args[0];
-            var referencesToRemove = args.Skip(1).ToList();
-
-            var project = new Project();
-            project.Load(projectFilePath);
-
-            var allItems = AllItemGroupsIn(project).SelectMany(group => group.Cast<BuildItem>());
-            var projectReferences = allItems.Where(IsProjectReference);
-
-            var toRemove = projectReferences.Where(item => referencesToRemove.Contains(item.FinalItemSpec)).ToList();
             
-            foreach (var item in toRemove)
-            {
-                project.RemoveItem(item);
-            }
 
-            if (!toRemove.Any())
+            if (args[0] == "remove")
             {
-                Console.Error.WriteLine("Could not remove '{0}' from '{1}'", referencesToRemove, projectFilePath);
-                return -1;
-            }
+                var projectFilePath = args[1];
+                var referencesToRemove = args.Skip(2).ToList();
 
-            project.Save(projectFilePath);
+                var project = new Project();
+                project.Load(projectFilePath);
+
+                var allItems = AllItemGroupsIn(project).SelectMany(group => group.Cast<BuildItem>());
+                var projectReferences = allItems.Where(IsProjectReference);
+
+                var toRemove = projectReferences.Where(item => referencesToRemove.Contains(item.FinalItemSpec)).ToList();
+
+                foreach (var item in toRemove)
+                {
+                    project.RemoveItem(item);
+                }
+
+                if (!toRemove.Any())
+                {
+                    Console.Error.WriteLine("Could not remove '{0}' from '{1}'", referencesToRemove, projectFilePath);
+                    return -1;
+                }
+
+                if (project.IsDirty)
+                {
+                    project.Save(projectFilePath);
+                }
+
+            }
+            else if (args[0] == "add")
+            {
+                var projectFilePath = args[1];
+                var assemblyPath = args[2];
+
+                var project = new Project();
+                project.Load(projectFilePath);
+
+                var group = AllItemGroupsIn(project).FirstOrDefault(g => g.Cast<BuildItem>().Any(i => i.Name == "Reference")) ??
+                            project.AddNewItemGroup();
+
+                
+                var item = group.AddNewItem("Reference", Path.GetFileNameWithoutExtension(assemblyPath));
+                var hintPath = RelativePath.MakeRelativePath(projectFilePath, assemblyPath);
+                item.SetMetadata("SpecificVersion", "False");
+                item.SetMetadata("HintPath", hintPath);
+
+                if (project.IsDirty)
+                {
+                    project.Save(projectFilePath);
+                }
+            }
             return 0;
         }
 
---
+++ CSProj-Tool/RelativePath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace csproj.tool
+{
+    static class RelativePath
+    {
+        public static String MakeRelativePath(String fromPath, String toPath)
+        {
+            if (String.IsNullOrEmpty(fromPath)) throw new ArgumentNullException("fromPath");
+            if (String.IsNullOrEmpty(toPath)) throw new ArgumentNullException("toPath");
+
+            Uri fromUri = new Uri(Path.GetFullPath(fromPath));
+            Uri toUri = new Uri(Path.GetFullPath(toPath));
+
+            if (fromUri.Scheme != toUri.Scheme) { return toPath; } // path can't be made relative.
+
+            Uri relativeUri = fromUri.MakeRelativeUri(toUri);
+            String relativePath = Uri.UnescapeDataString(relativeUri.ToString());
+
+            if (toUri.Scheme.ToUpperInvariant() == "FILE")
+            {
+                relativePath = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+            }
+
+            return relativePath;
+        }
+    }
+}
